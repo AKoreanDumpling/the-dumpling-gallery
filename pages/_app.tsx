b0ce489fa28dd9a2837a0236bf9a6fca6685c5d5
Nathan Mah <187478035+AKoreanDumpling@users.noreply.github.com>
2025-12-02 20:28:36 -0800

    Add fade animation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,36 @@
-import type { AppProps } from "next/app";
 import "../styles/index.css";
+import type { AppProps } from "next/app";
+import { motion, AnimatePresence } from "framer-motion";
+import { useRouter } from "next/router";
 
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }: AppProps) {
+	const router = useRouter();
+	return (
+		<AnimatePresence mode="wait">
+			<motion.div
+				key={router.route}
+				initial="initialState"
+				animate="animateState"
+				exit="exitState"
+				transition={{
+					duration: 0.75,
+				}}
+				variants={{
+					initialState: {
+						opacity: 0,
+					},
+					animateState: {
+						opacity: 1,
+					},
+					exitState: {
+						opacity: 0,
+					},
+				}}
+			>
+				<Component {...pageProps} />
+			</motion.div>
+		</AnimatePresence>
+	);
 }
+
+export default MyApp;
