07007a6d2f87425a2017fcdc057d00d7160f5676
Nathan Mah <187478035+AKoreanDumpling@users.noreply.github.com>
2025-12-02 17:25:49 -0800

    Revert "Install Vercel Speed Insights for Next.js"
    This reverts commit 5bdeb9c40fa177c2a51bf967b791c9ad0ac33d24.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,6 @@
 import type { AppProps } from "next/app";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 import "../styles/index.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Component {...pageProps} />
-      <SpeedInsights />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
