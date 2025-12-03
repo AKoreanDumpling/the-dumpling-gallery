37b06c50a154db060386f5de9b842c2983ce9c23
Nathan Mah <187478035+AKoreanDumpling@users.noreply.github.com>
2025-12-02 22:07:51 -0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -60,7 +60,7 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
 							The Dumpling Gallery
 						</h1>
 						<p className="max-w-[40ch] text-white/75 sm:max-w-[32ch]">
-							View and download full resolution photos
+							View and download full resolution photos! (best experienced on desktop)
 						</p>
 						<a
 							className="pointer z-10 mt-6 rounded-lg border border-white bg-white px-3 py-2 text-sm font-semibold text-black transition hover:bg-white/10 hover:text-white md:mt-4"
