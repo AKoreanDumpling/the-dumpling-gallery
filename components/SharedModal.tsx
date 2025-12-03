0181db4dff4d65b665edbd00ab391b9150f26aa6
Nathan Mah <187478035+AKoreanDumpling@users.noreply.github.com>
2025-12-02 17:47:46 -0800

    Modified more little details

--- components/SharedModal.tsx
+++ components/SharedModal.tsx
@@ -78,7 +78,7 @@ export default function SharedModal({
 									width={navigation ? 1280 : 1920}
 									height={navigation ? 853 : 1280}
 									priority
-									alt="Next.js Conf image"
+									alt="Main image"
 									onLoad={() => setLoaded(true)}
 								/>
 							</motion.div>
