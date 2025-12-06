1d009352c79773c6597562abc55a6b2db41a705d
Nathan Mah <187478035+AKoreanDumpling@users.noreply.github.com>
2025-12-06 00:22:32 -0800

    Update down.tsx

--- pages/down.tsx
+++ pages/down.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
 
 				<div className="display-grid place-items-center">
 
-					<div className="after:content relative h-[629px] w-1/4 mb-5 flex flex-col items-center justify-end gap-4 overflow-hidden rounded-lg bg-white/10 px-6 pb-16 pt-64 text-center text-white shadow-highlight after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:shadow-highlight lg:pt-0">
+					<div className="after:content relative h-[629px] w-3/4 mb-5 flex flex-col items-center justify-end gap-4 overflow-hidden rounded-lg bg-white/10 px-6 pb-16 pt-64 text-center text-white shadow-highlight after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:shadow-highlight lg:pt-0">
 						<div className="absolute inset-0 flex items-center justify-center opacity-20">
 							<span className="flex max-h-full max-w-full items-center justify-center">
 								<Bridge />
@@ -22,11 +22,11 @@ const Home: NextPage = () => {
 							<span className="absolute left-0 right-0 bottom-0 h-[250px] bg-gradient-to-b from-black/0 via-black to-black"></span>
 						</div>
 						<h1 className="mt-8 mb-2 text-base font-bold uppercase tracking-widest">
-							503<br />The Dumpling Gallery is down for Maintenance
+							503:<br />The Dumpling Gallery is down for Maintenance
 						</h1>
 
 						<p className="max-w-[40ch] text-white/75 sm:max-w-[32ch]">
-							We will be back online shortly with uodated photos, thank you for your patience!
+							We will be back online shortly with uodated photos, thank you for your patience! (HTTP 503: Service Unavailable)
 						</p>
 					</div>
 				</div>
